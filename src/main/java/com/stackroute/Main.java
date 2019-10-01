f40cd1279fb6a4507c6bb820eb254a886a7aa1cd
cazzprogrammer <dminj97@gmail.com>
2019-10-01 16:18:11 +0530

    First commit - Implemented BeanlifecycledemoBean

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -4,31 +4,23 @@ package com.stackroute;
  * Hello world!
  *
  */
-import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
+import com.stackroute.demo.BeanLifecycleDemoBean;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+
+
 
 public class Main
 {
     public static void main( String[] args )
     {
-        //Using Applicationcontext
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie mob = (Movie)context.getBean("movie");
-        System.out.println(mob.toString());
 
-        //ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
-        Movie mob1 = (Movie)context.getBean("movieA");
-        System.out.println(mob1.toString());
 
+        AbstractApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+        BeanLifecycleDemoBean beanLifecycleDemoBean = (BeanLifecycleDemoBean)context1.getBean("BeanLifecycleDemoBean");
+        System.out.println(beanLifecycleDemoBean.toString());
+
+        context1.registerShutdownHook();
 
     }
 }
